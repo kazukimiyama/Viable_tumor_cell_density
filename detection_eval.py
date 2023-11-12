f6b82b87a8d561f663ba55aa78ca279eb81a1453
kazukimiyama <51731898+kazukimiyama@users.noreply.github.com>
2023-11-12 16:54:24 +0900

    Add files via upload

---
+++ detection_eval.py
@@ -0,0 +1,29 @@
+import torch.nn as nn
+import torch
+import cv2
+import numpy as np
+
+
+def eval_net(
+    net, dataset, gpu=True, vis=None, vis_im=None, vis_gt=None, loss=nn.MSELoss()
+):
+    criterion = loss
+    net.eval()
+    losses = 0
+    torch.cuda.empty_cache()
+    for iteration, data in enumerate(dataset):
+        img = data["image"]
+        target = data["gt"]
+        if gpu:
+            img = img.cuda()
+            target = target.cuda()
+
+        pred_img = net(img)
+
+        loss = criterion(pred_img, target)
+        losses += loss.data
+    pred_img = pred_img.detach().cpu().numpy()
+    cv2.imwrite("conf.tif", (img.detach().cpu().numpy() * 255).astype(np.uint8)[0].transpose(1, 2, 0))
+    cv2.imwrite("conf_gt.tif", (target.detach().cpu().numpy() * 255).astype(np.uint8)[0].transpose(1, 2, 0))
+    cv2.imwrite("conf_eval.tif", (pred_img * 255).astype(np.uint8)[0, 0])
+    return losses / (iteration + 1)
